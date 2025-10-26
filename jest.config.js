e35951fc1e03d188e6400b020867827df1085c21
Ilya Lomtev <lomtev@yandex-team.ru>
2025-10-26 13:03:15 +0300

    fix(tests): fix unit test pattern

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     verbose: true,
     moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
     rootDir: '.',
+    testMatch: ['**/*.unit.test.ts', '**/*.unit.test.tsx'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
