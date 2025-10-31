b616e7236a8bb6358e2c6493bc57cb52e851a104
Viacheslav Zinovev <vdzin@yandex-team.ru>
2025-10-31 16:53:15 +0300

    feat: add ToolIndicator component

---
+++ src/components/atoms/ToolIndicator/__tests__/ToolIndicator.visual.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {test} from '~playwright/core';
+
+import {ToolIndicatorStories} from './helpersPlaywright';
+
+test.describe('ToolIndicator', {tag: '@ToolIndicator'}, () => {
+    test('should render success state', async ({mount, expectScreenshot}) => {
+        await mount(<ToolIndicatorStories.Success />);
+
+        await expectScreenshot();
+    });
+    test('should render error state', async ({mount, expectScreenshot}) => {
+        await mount(<ToolIndicatorStories.Error />);
+
+        await expectScreenshot();
+    });
+    test('should render info state', async ({mount, expectScreenshot}) => {
+        await mount(<ToolIndicatorStories.Info />);
+
+        await expectScreenshot();
+    });
+});
