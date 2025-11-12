d2c333533e273f211c0ad7896db9fc24874490f5
Ilya Lomtev <lomtev@yandex-team.ru>
2025-11-12 12:58:49 +0300

    fixup! feat(history): add new history chat

--- src/components/atoms/ChatDate/__stories__/ChatDate.stories.tsx
+++ src/components/atoms/ChatDate/__stories__/ChatDate.stories.tsx
@@ -159,7 +159,7 @@ export const DifferentDateTypes: StoryObj<ChatDateProps> = {
 
 export const RecentDates: StoryObj<ChatDateProps> = {
     render: (args) => {
-        const today = new Date('2025-11-11');
+        const today = new Date('2025-11-10');
 
         const getTitle = (daysAgo: number) => {
             return daysAgo ? `[ + ${daysAgo} ]` : `[ 0 ]`;
