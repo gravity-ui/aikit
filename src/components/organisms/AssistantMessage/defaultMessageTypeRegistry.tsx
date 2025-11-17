a9c4233d5b8dfa0f3249572e91f300cd2e94f4af
Viacheslav Zinovev <vdzin@yandex-team.ru>
2025-11-17 20:17:42 +0300

    feat(AssistantMessage): register ThinkingMessage renderer for 'thinking' message type

--- src/components/organisms/AssistantMessage/defaultMessageTypeRegistry.tsx
+++ src/components/organisms/AssistantMessage/defaultMessageTypeRegistry.tsx
@@ -1,12 +1,13 @@
 import type {OptionsType} from '@diplodoc/transform/lib/typings';
 
-import type {TextMessagePart, ToolMessagePart} from '../../../types/messages';
+import type {TextMessagePart, ThinkingMessagePart, ToolMessagePart} from '../../../types/messages';
 import {
     type MessageRendererRegistry,
     createMessageRendererRegistry,
     registerMessageRenderer,
 } from '../../../utils/messageTypeRegistry';
 import {MarkdownRenderer} from '../../atoms/MarkdownRenderer';
+import {ThinkingMessage} from '../ThinkingMessage';
 import {ToolMessage} from '../ToolMessage';
 
 export function createDefaultMessageRegistry(
@@ -22,6 +23,10 @@ export function createDefaultMessageRegistry(
 
     registerMessageRenderer<ToolMessagePart>(registry, 'tool', {
         component: ({part}) => <ToolMessage {...part.data} />,
+    });
+
+    registerMessageRenderer<ThinkingMessagePart>(registry, 'thinking', {
+        component: ({part}) => <ThinkingMessage data={part.data} />,
     });
 
     return registry;
