502101cde83df5970c8c1ddd29070a63248a6ff2
Victor Trumpel <vtrumpel@10.211.70.195-vpn.dhcp.yndx.net>
2026-02-10 11:26:10 +0300

    feat(OpenAIService): add possible set default settings

--- src/server/openai/OpenAIService.ts
+++ src/server/openai/OpenAIService.ts
@@ -1,19 +1,46 @@
-import {OpenAI} from 'openai/client.js';
+import {ClientOptions, OpenAI} from 'openai/client.js';
 import {ResponseCreateParamsStreaming} from 'openai/resources/responses/responses.js';
 
 import {ResponseStream} from './ResponseStream.js';
 
+type ExtraClientOptions = {
+    model?: string;
+    agent?: string;
+};
+
 export class OpenAIService extends OpenAI {
-    async createResponseStream(body: ResponseCreateParamsStreaming) {
+    private model?: string;
+    private agent?: string;
+
+    constructor(options: ClientOptions & ExtraClientOptions) {
+        super(options);
+
+        this.model = options.model;
+        this.agent = options.agent;
+    }
+
+    async createResponseStream(payload: ResponseCreateParamsStreaming) {
         const abortController = new AbortController();
 
+        const metadata = this.getMetadataForResponseStream(payload.metadata);
+
         const stream = await this.responses.create(
-            {...body, stream: true},
+            {model: this.model, ...payload, stream: true, metadata},
             {signal: abortController.signal},
         );
 
         const responseStream = new ResponseStream(stream, abortController);
 
         return responseStream;
+    }
+
+    private getMetadataForResponseStream(
+        payloadMetadata: ResponseCreateParamsStreaming['metadata'],
+    ) {
+        if (payloadMetadata) return payloadMetadata;
+
+        if (this.agent) return {agent: this.agent};
+
+        return undefined;
     }
 }
