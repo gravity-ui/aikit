940b83ecc56990ede78484f16e44d17f6e86f9b0
Anton Standrik <astandrik@yandex-team.ru>
2026-07-03 10:35:01 +0300

    feat(PromptInput): expose textarea control ref (#195)
    * feat(PromptInput): expose textarea control ref
    
    * fix(PromptInput): rename textarea ref prop
    
    ---------
    
    Co-authored-by: Anton Standrik <astandrik@qavm-62293b9d.qemu>

--- src/components/organisms/PromptInput/PromptInputFull.tsx
+++ src/components/organisms/PromptInput/PromptInputFull.tsx
@@ -48,6 +48,7 @@ export function PromptInputFull(props: PromptInputFullProps) {
         minRows = 1,
         maxRows = 15,
         autoFocus = false,
+        inputRef,
         qa: bodyQa,
     } = bodyProps;
 
@@ -90,6 +91,7 @@ export function PromptInputFull(props: PromptInputFullProps) {
                 minRows={minRows}
                 maxRows={maxRows}
                 autoFocus={autoFocus}
+                ref={inputRef}
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
                 inputClassName={b('textarea')}
